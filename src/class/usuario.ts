ff1a9d60d683c51830d0d4712caf9e069685069b
Alvaro Ribeiro Pereira <alvaro.r.p@hotmail.com>
2021-03-04 21:24:30 -0300

    Exemplo de constructor

--- src/class/usuario.ts
+++ src/class/usuario.ts
@@ -4,4 +4,8 @@ export class Retangulo {
   id: ObjectID;
   altura: number;
   largura: number;
+  // constructor(id: ObjectID, altura: number, largura: number) {
+  //   this.altura = altura;
+  //   this.largura = largura;
+  // }
 }
